d6e4d882acb032474a417894348358b086d5ff0b
myselfrabin <shivagaire839@gmail.com>
2024-01-08 17:19:00 +0545

    np.3y add garako

---
+++ Number_pattern.py/np3.py
@@ -0,0 +1,5 @@
+rows = int (input ("Please enter how many rows you need: "))  
+for i in range (rows+1):  
+    for j in range (i):  
+        print(i,end = " ")  
+    print () #for printing a new line
